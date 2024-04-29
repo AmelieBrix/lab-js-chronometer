9d068f87a775f283afd01d10dcf06e9f70e52d77
Amelie Brix <brix.amelie@googlemail.com>
2024-04-29 22:04:21 +0200

    Lab chronometer done

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,52 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
-  }
+    this.currentTime = 0;
+    this.intervalId = null;
 
-  start(printTimeCallback) {
-    // ... your code goes here
   }
+  start(printTimeCallback) {
+    this.intervalId = setInterval(() => {
+      this.currentTime++;
+      if (typeof printTimeCallback !== 'undefined') {
+        printTimeCallback()
+      }
+    }, 1000);
+  };
+
+  // myChronometer.start(function(){console.log(myChronometer.split())})
 
   getMinutes() {
+    return Math.floor(this.currentTime / 60);
     // ... your code goes here
   }
 
   getSeconds() {
+    return this.currentTime % 60
     // ... your code goes here
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    return ("0" + value.toString()).slice(-2);
   }
+  // ... your code goes here
 
   stop() {
+    clearInterval(this.intervalId)
     // ... your code goes here
   }
 
   reset() {
+    this.currentTime = 0;
     // ... your code goes here
   }
 
   split() {
+    return `${this.computeTwoDigitNumber(this.getMinutes())}:${this.computeTwoDigitNumber(this.getSeconds())}`;
     // ... your code goes here
   }
+
 }
+
+let myChronometer = new Chronometer();
+
+myChronometer.start(function(){console.log(myChronometer.split())})
